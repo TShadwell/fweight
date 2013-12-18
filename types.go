bead655aa2e7c9c4f3839d8d0e3ea24667c91806
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2013-12-18 15:26:30 +0000

    Remove cruft

--- types.go
+++ types.go
@@ -9,31 +9,8 @@ type (
 	//for this http.Request.
 	//
 	//The routing terminates when Router is Handler, or if
-	//Router is nil; a nil value causes NotFound to be served
-	//by the server.
+	//Router is nil.
 	Router interface {
 		RouteHTTP(*http.Request) Router
 	}
-
-	PathingRouter interface {
-		Child(subpath string) (n Router, remainingSubpath string)
-	}
-
-	ExtraFunctions interface {
-		HandleHTTPError(e error, rw http.ResponseWriter, rq *http.Request)
-	}
-
-	ResponseWriter interface {
-		http.ResponseWriter
-		//The Server's HandleHTTPError function.
-		ExtraFunctions
-	}
 )
-
-type (
-	MarshalFunc func(i interface{}) ([]byte, error)
-)
-
-func (m MarshalFunc) Marshal(i interface{}) ([]byte, error) {
-	return m(i)
-}
