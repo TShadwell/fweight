80bb1a6508ade351354cff8dfb6a31f0d41a6c99
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2014-02-21 13:53:59 +0000

    Add convienience function to ignore request in getter

--- object/getter.go
+++ object/getter.go
@@ -13,3 +13,11 @@ type GetterFunc func(*http.Request) interface{}
 func (g GetterFunc) Get(r *http.Request) interface{} {
 	return g(r)
 }
+
+func NewGetter(g interface{
+	Get() interface{}
+}) Getter {
+	return GetterFunc(func(_ *http.Request) interface{}{
+		return g.Get()
+	})
+}
