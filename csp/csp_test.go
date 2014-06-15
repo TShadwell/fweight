6193288c4da37439557e0c3b9750f7b87a7b6a47
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2014-06-15 05:23:01 +0100

    example

--- csp/csp_test.go
+++ csp/csp_test.go
@@ -1,6 +1,7 @@
 package csp
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,4 +29,42 @@ func TestWorks(t *testing.T) {
 		t.Fatal("No CSP set!")
 	}
 
+}
+
+func ExampleContentSecurityPolicy_Middleware() {
+	csp := ContentSecurityPolicy{
+		Default: Self,
+		Style: Sources(
+			Self,
+			"fonts.googleapis.com",
+			UnsafeInline,
+		),
+		Script: Sources(
+			Self,
+			UnsafeInline,
+		),
+		Font: Sources(
+			Self,
+			Data,
+			"themes.googleusercontent.com",
+		),
+		Sandbox: Exceptions(
+			AllowForms,
+			AllowSameOrigin,
+			AllowScripts,
+		),
+	}
+
+	h := csp.Middleware(http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {}))
+
+	req, err := http.NewRequest("GET", "http://example.com/foo", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	w := httptest.NewRecorder()
+	h.ServeHTTP(w, req)
+
+	fmt.Print(w.Header().Get("Content-Security-Policy"))
+	// Output: default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' fonts.googleapis.com 'unsafe-inline'; font-src 'self' data: themes.googleusercontent.com; sandbox allow-forms allow-same-origin allow-scripts
 }
